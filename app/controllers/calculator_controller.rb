9d21698515fc629849ac57af520c6e305f945119
eli block <eliblock@yahoo.com>
2017-01-29 12:20:05 -0800

    profit calculator

--- app/controllers/calculator_controller.rb
+++ app/controllers/calculator_controller.rb
@@ -1,4 +1,11 @@
 class CalculatorController < ApplicationController
   def new
   end
+
+  def calculate
+    @revenue = params[:revenue].to_i
+    @expense = params[:expense].to_i
+    @profit = @revenue - @expense
+    render_to_string 'new'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root 'calculator#new'
+  get '/calculate', to: 'calculator#calculate'
 end
